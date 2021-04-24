b97bf33cb82aea21fc2bbdfad004e17b793cbbd8
ESPINO CAMPOS,JUAN <jespinca@gruposantander.com>
2021-04-24 02:16:05 +0200

    first commit

---
+++ src/main/java/com/example/demo/WeatherController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/weather")
+public class WeatherController {
+
+    private final WeatherRepository weatherRepository;
+
+    public WeatherController(WeatherRepository weatherRepository) {
+        this.weatherRepository = weatherRepository;
+    }
+
+    @GetMapping("/city")
+    public @ResponseBody Weather getWeatherForCity(@RequestParam("name") String cityName) {
+        return weatherRepository.findById(cityName).map(weather -> {
+            weather.setDescription("It's always sunny on Azure Spring Cloud");
+            weather.setIcon("weather-sunny");
+            return weather;
+        }).get();
+    }
+}
